1757fc612cf934860b1a59338038df578b3986ff
Phillip Goldman <philgoldman@Phillips-MBP.attlocal.net>
2020-09-14 12:00:34 -0400

    Updates

---
+++ precache-manifest.c549b95b209e52884c4ee645ec6bd4d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05722ff740aeb6aed2a7cef5d866b8c",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "1b140dc474416354b680",
+    "url": "/netflix-clone/static/css/main.da33eaad.chunk.css"
+  },
+  {
+    "revision": "a7f3cf3538b74363cc17",
+    "url": "/netflix-clone/static/js/2.3fa0a5a2.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.3fa0a5a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b140dc474416354b680",
+    "url": "/netflix-clone/static/js/main.8ab26975.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
